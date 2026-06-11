d12f2bff912db750f06b96cbd1b3e62b165600da
SalmaDanane <343249595@tdsb.ca>
2026-06-11 10:03:19 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,25 @@ public class PracticeProblem {
 
 	}
 
+	public static boolean hasCapital(String word) {
+			if (word.toLowerCase() == word) { //checking for lowercase
+				return false;
+			}
+        return true;
+		}
+
+	public static boolean isPrime(int number) {
+		if (number == 1 ) { // isnt a prime
+				return false;
+			}
+		for (int i = 2; i < number; i++) {
+			if (number % i == 0){
+				return false;
+			}
+		}
+		return true;
+	}
+
+
 }
+
